42012a751633290b5c7ecade51b9a9c480a912b5
Breijen <bryan.laanvd@gmail.com>
2026-05-24 12:53:32 +0200

    Add OGG and FLAC support to FileSource
    Unsupported extensions return AireError::FileExtNotSupported.

--- src/error.rs
+++ src/error.rs
@@ -6,12 +6,15 @@ pub enum AireError {
     /// This typically means commands are being sent faster than the audio
     /// thread can process them. The buffer holds 256 commands.
     CommandBufferFull,
+    /// Sound file extension is not supported (yet).
+    FileExtNotSupported(String)
 }
 
 impl std::fmt::Display for AireError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             AireError::CommandBufferFull => write!(f, "command buffer full"),
+            AireError::FileExtNotSupported(ext) => write!(f, "unsupported format: .{}", ext),
         }
     }
 }
